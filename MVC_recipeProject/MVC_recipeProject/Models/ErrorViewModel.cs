6e6b02196ef85dfdaa67c0b9df586117d8e0fb12
Matan <jerbimatan@gmail.com>
2024-09-01 10:06:05 +0300

    first commit

---
+++ MVC_recipeProject/MVC_recipeProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_recipeProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
