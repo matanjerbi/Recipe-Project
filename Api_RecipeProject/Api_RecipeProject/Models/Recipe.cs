796c9f5869ad3dc4ef9bbe3d2c69f47ec5e28cff
Matan <jerbimatan@gmail.com>
2024-09-01 11:28:14 +0300

    Add files via upload
    first API

---
+++ Api_RecipeProject/Api_RecipeProject/Models/Recipe.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api_RecipeProject.Models
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Components { get; set; }
+        public string Preparation { get; set; }
+         
+    }
+}
--- Api_RecipeProject/Api_RecipeProject/obj/Debug/net8.0/Api_RecipeProject.AssemblyInfo.cs
+++ Api_RecipeProject/Api_RecipeProject/obj/Debug/net8.0/Api_RecipeProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api_RecipeProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e6b02196ef85dfdaa67c0b9df586117d8e0fb12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api_RecipeProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api_RecipeProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
